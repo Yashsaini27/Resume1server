d702ba5f88ae77a8271ec3abe24a0e59a8afe491
Yashsaini27 <90746071+Yashsaini27@users.noreply.github.com>
2024-07-30 21:11:23 +0530

    Add files via upload

---
+++ controllers/taskController.js
@@ -0,0 +1,45 @@
+import Task from '../models/task.js';  // Adjust the import path
+
+export const createTask = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+export const updateTask=async(req,res)=>{
+  try{
+    const{id}=req.params;
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+ 
+
+// Delete task by ID
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
